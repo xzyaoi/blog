b8ffa8221300f827ec8875191be3689cfb148044
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2020-04-08 00:00:51 +0200

    update sw

--- sw.js
+++ sw.js
@@ -31,9 +31,7 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "blog.yaonotes.org"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
